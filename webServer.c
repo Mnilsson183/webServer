c6dcfe7fdada07b87cd5d662b96d4597db476951
mnilsson183 <morgan.nilsson7111@icloud.com>
2023-09-20 23:29:32 -0600

    commit

--- webServer.c
+++ webServer.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -14,6 +15,7 @@ int main(void){
         perror("The server was unable to start the socket");
         exit(EXIT_FAILURE);
     }
+    printf("Socket created successfully\n");
 
     // set up the server addr 
     struct sockaddr_in serverAddr;
@@ -23,51 +25,60 @@ int main(void){
 
     // bind together the socket and the addr
     if (bind(serverSocket, (struct serverAddr *)&serverAddr, sizeof(serverAddr))){
-        perror("The server was unable to bind the server");
+        perror("The address was unable to bind the server");
         exit(EXIT_FAILURE);
     }
+    printf("The Address was successfully bound\n");
 
     // began to listen for client connections
     if (listen(serverSocket, 5) == -1){
         perror("The server was unable to listen");
         exit(EXIT_FAILURE);
     }
+    printf("Server listening for connections\n");
 
     //allow a client connection
     int clientSocket;
     struct sockaddr_in clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
 
-    clientSocket = accept(serverSocket, (struct serverAddr *)&serverAddr, sizeof(serverAddr));
-    if (clientSocket == -1){
-        perror("The server was unable to accept a client connection");
-        exit(EXIT_FAILURE);
+    while(true){
+        clientSocket = accept(serverSocket, (struct serverAddr *)&serverAddr, sizeof(serverAddr));
+        if (clientSocket == -1){
+            perror("The server was unable to accept a client connection");
+            exit(EXIT_FAILURE);
+        }
+        printf("Connection Accepted\n");
+
+        // parse the http request
+
+
+        int contentLength;
+
+        // serve web page
+        FILE *file = fopen("~/Desktop/webServer", "r");
+        if (file == NULL){
+            perror("The web page to serve was not found");
+            return -1;
+        }
+
+        char responseHeader[1024];
+        sprintf(responseHeader, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %d\r\n\r\n", contentLength);
+        send(clientSocket, sizeof(responseHeader), strlen(responseHeader), 0);
+
+        char buffer[1024];
+        while( fgets(buffer, sizeof(buffer),file) != NULL){
+            send(clientSocket, buffer, strlen(buffer), 0);
+        }
+
     }
-
-    // parse the http request
-
-
-    int contentLength;
-
-    // serve web page
-    FILE *file = fopen("~/Desktop/webServer", "r");
-    if (file == NULL){
-        perror("The web page to serve was not found");
-        return -1;
-    }
-
-    char responseHeader[1024];
-    sprintf(responseHeader, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %d\r\n\r\n", contentLength);
-    send(clientSocket, sizeof(responseHeader), strlen(responseHeader), 0);
-
-    char buffer[1024];
-    while( fgets(buffer, sizeof(buffer),file) != NULL){
-        send(clientSocket, buffer, strlen(buffer), 0);
-    }
-
     // close the client connection
     close(clientSocket);
 
     // close the server
     close(serverSocket);
+}
+
+void *handle_client(void *arg){
+    
 }
